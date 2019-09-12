d4c729417f6bea3653fa0de0fd39f7e59fe300db
Wildape <Paul Wilda>
2019-09-12 00:34:47 -0400

    Removed size limitation

--- hw01/README.md
+++ hw01/README.md
@@ -6,8 +6,7 @@ Controls
 	q	:Quit
 
 Options
-	--size (must be less than 11)
+	--size
 
 I have completed all 6 tasks
-The etch-A-Sketch is run using   ./etchasketch.py --size
-with a size option of less than 11 
+The etch-A-Sketch is run using   ./etchasketch.py --size 
--- hw01/etchAsketch.py
+++ hw01/etchAsketch.py
@@ -5,12 +5,10 @@ import argparse
 def setup_stage(args, stdscr):
 	x_size = " "
 	for x in range(args.size):
-		x_size = x_size + " " + str(x)
-		if x < 10:
-			stdscr.addstr(2+x, 0, str(x)+" :")
-		else:
-			stdscr.addstr(2+x, 0, str(x)+":")
-	stdscr.addstr(1, 1, x_size)
+		x_size = x_size + "__"
+		stdscr.addstr(3+x, 0, "|")
+		
+	stdscr.addstr(2, 0, x_size)
 	stdscr.refresh()
 	
 def main(stdscr):
@@ -19,13 +17,11 @@ def main(stdscr):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--size",type=int)
 	args = parser.parse_args()
-	if args.size > 10:
-		exit()
 	setup_stage(args, stdscr)
-	xcur = 2
-	ycur = 3
+	xcur = 3
+	ycur = 2
 	curses.curs_set(0)
-	
+	stdscr.addstr(0,0,"Movement: W A S D Restart: r Quit: q",curses.A_BOLD)
 	while True:
 		stdscr.addstr(xcur,ycur,"X",curses.A_BOLD)
 		key = stdscr.getkey()
@@ -33,7 +29,7 @@ def main(stdscr):
 			break
 		if key == 'w':
 			stdscr.addstr(xcur,ycur,"X")
-			if xcur > 2:
+			if xcur > 3:
 				xcur -= 1
 		if key == 'a':
 			stdscr.addstr(xcur,ycur,"X")
@@ -41,7 +37,7 @@ def main(stdscr):
 				ycur -= 2
 		if key == 's':
 			stdscr.addstr(xcur,ycur,"X")
-			if xcur < args.size+1:
+			if xcur < args.size+2:
 				xcur += 1
 		if key == 'd':
 			stdscr.addstr(xcur,ycur,"X")
