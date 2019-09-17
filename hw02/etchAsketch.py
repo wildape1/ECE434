fb5487d1f199c0e88c471bc36a8e0a602e178b69
Wildape <Paul Wilda>
2019-09-16 23:31:05 -0400

    Edit etchAsketch

--- hw02/etchAsketch.py
+++ hw02/etchAsketch.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
-import Adafruit_BBIO.GPIO as GPIO
-import time
 import curses
 import argparse
 
-def setup_stage(args, stdscr):
-	x_size = " "
-	for x in range(args.size):
-		x_size = x_size + "__"
-		stdscr.addstr(3+x, 0, "|")
-		
-	stdscr.addstr(2, 0, x_size)
-	stdscr.refresh()
-
+import Adafruit_BBIO.GPIO as GPIO
+import time
 set = 1000
 up = "P8_14"
 down = "P8_16"
@@ -28,65 +19,87 @@ GPIO.setup(left, GPIO.IN)
 GPIO.setup(right, GPIO.IN)
 GPIO.setup(reset, GPIO.IN)
 
-# Clear screen
-stdscr.clear()
-parser = argparse.ArgumentParser()
-parser.add_argument("--size",type=int)
-args = parser.parse_args()
-setup_stage(args, stdscr)
-xcur = 3
-ycur = 2
-curses.curs_set(0)
-stdscr.addstr(0,0,"Movement: W A S D Restart: r Quit: q",curses.A_BOLD)
+GPIO.setup("P8_7", GPIO.OUT)
+GPIO.setup("P8_8", GPIO.OUT)
+GPIO.setup("P8_9", GPIO.OUT)
+GPIO.setup("P8_10", GPIO.OUT)
 
+def setup_stage(args, stdscr):
+	x_size = " "
+	for x in range(args.size):
+		x_size = x_size + "__"
+		stdscr.addstr(3+x, 0, "|")
+		
+	stdscr.addstr(2, 0, x_size)
+	stdscr.refresh()
+	
+def main(stdscr):
+	# Clear screen
+	stdscr.clear()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--size",type=int)
+	args = parser.parse_args()
+	setup_stage(args, stdscr)
+	xcur = 3
+	ycur = 2
+	curses.curs_set(0)
+	stdscr.addstr(0,0,"Movement: W A S D Restart: r Quit: q",curses.A_BOLD)
+	while True:
+		stdscr.addstr(xcur,ycur,"X",curses.A_BOLD)
+		#key = stdscr.getkey()
+		if key == 'q':
+			break
+		if key == 'w':
+			stdscr.addstr(xcur,ycur,"X")
+			if xcur > 3:
+				xcur -= 1
+		if key == 'a':
+			stdscr.addstr(xcur,ycur,"X")
+			if ycur > 3:
+				ycur -= 2
+		if key == 's':
+			stdscr.addstr(xcur,ycur,"X")
+			if xcur < args.size+2:
+				xcur += 1
+		if key == 'd':
+			stdscr.addstr(xcur,ycur,"X")
+			if ycur < args.size*2:
+				ycur += 2
+		if key == 'r':
+			main(stdscr)
+		stdscr.refresh()
 
 def update(status):
 
 	if status == up:
-		key = 'w'	
+		GPIO.output("P8_7", GPIO.HIGH)
+		key = 'w'
 
 	if status == down:
+		GPIO.output("P8_8", GPIO.HIGH)
 		key = 's'
 
 	if status == left:
+		GPIO.output("P8_9", GPIO.HIGH)
 		key = 'a'
 
 	if status == right:
+		GPIO.output("P8_10", GPIO.HIGH)
 		key = 'd'
 
 	if status == reset:
+		print("reset")
+		GPIO.output("P8_7", GPIO.LOW)
+		GPIO.output("P8_8", GPIO.LOW)
+		GPIO.output("P8_9", GPIO.LOW)
+		GPIO.output("P8_10", GPIO.LOW)
 		key = 'r'
-		
 
-update(set)
 GPIO.add_event_detect(up, GPIO.RISING, bouncetime=200, callback=update)
 GPIO.add_event_detect(down, GPIO.RISING, bouncetime=200, callback=update)
 GPIO.add_event_detect(right, GPIO.RISING, bouncetime=200, callback=update)
 GPIO.add_event_detect(left, GPIO.RISING, bouncetime=200, callback=update)
 GPIO.add_event_detect(reset, GPIO.RISING, bouncetime=200, callback=update)
 
-while True:
-	stdscr.addstr(xcur,ycur,"X",curses.A_BOLD)
-	key = stdscr.getkey()
-	if key == 'q':
-			break
-	if key == 'w':
-		stdscr.addstr(xcur,ycur,"X")
-		if xcur > 3:
-			xcur -= 1
-	if key == 'a':
-		stdscr.addstr(xcur,ycur,"X")
-		if ycur > 3:
-			ycur -= 2
-	if key == 's':
-		stdscr.addstr(xcur,ycur,"X")
-		if xcur < args.size+2:
-			xcur += 1
-	if key == 'd':
-		stdscr.addstr(xcur,ycur,"X")
-		if ycur < args.size*2:
-			ycur += 2
-	if key == 'r':
-		main(stdscr)
-	stdscr.refresh()
+curses.wrapper(main)
 
