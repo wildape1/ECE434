ff69f1cca893af6007bf61a915216e410b886a16
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-09-16 22:40:03 -0400

    Finish LED and Button

--- hw02/LEDsandButtons.py
+++ hw02/LEDsandButtons.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-config-pin P9_14 gpio
-
 import Adafruit_BBIO.GPIO as GPIO
 import time
-
+set = 1000
 up = "P8_14"
 down = "P8_16"
 left = "P8_12"
 right = "P8_18"
-reset = "P8_45"
+reset = "P8_11"
 
 # Set up pins as inputs or outputs
 GPIO.setup(up, GPIO.IN)
@@ -23,37 +21,37 @@ GPIO.setup("P8_8", GPIO.OUT)
 GPIO.setup("P8_9", GPIO.OUT)
 GPIO.setup("P8_10", GPIO.OUT)
 
-reset = False
 
 def update(status):
 
 	if status == up:
-		GPIO.output(P8_7, GPIO.HIGH) = 1
+		GPIO.output("P8_7", GPIO.HIGH)
 
 	if status == down:
-		GPIO.output(P8_8, GPIO.HIGH) = 1
+		GPIO.output("P8_8", GPIO.HIGH)
 
 	if status == left:
-		GPIO.output(P8_9, GPIO.HIGH) = 1
+		GPIO.output("P8_9", GPIO.HIGH)
 
 	if status == right:
-		GPIO.output(P8_10, GPIO.HIGH) = 1
-	
+		GPIO.output("P8_10", GPIO.HIGH)
+
+	if status == status:
+		set = 1000
+
 	if status == reset:
-		reset = True
+		print("reset")
+		GPIO.output("P8_7", GPIO.LOW)
+		GPIO.output("P8_8", GPIO.LOW)
+		GPIO.output("P8_9", GPIO.LOW)
+		GPIO.output("P8_10", GPIO.LOW)
 
-	else  
-		GPIO.output(P8_7, GPIO.LOW) = 1
-		GPIO.output(P8_8, GPIO.LOW) = 1
-		GPIO.output(P8_9, GPIO.LOW) = 1
-		GPIO.output(P8_10, GPIO.LOW) = 1
+update(set)
+GPIO.add_event_detect(up, GPIO.RISING, bouncetime=200, callback=update)
+GPIO.add_event_detect(down, GPIO.RISING, bouncetime=200, callback=update)
+GPIO.add_event_detect(right, GPIO.RISING, bouncetime=200, callback=update)
+GPIO.add_event_detect(left, GPIO.RISING, bouncetime=200, callback=update)
+GPIO.add_event_detect(reset, GPIO.RISING, bouncetime=200, callback=update)
 
-while !reset:
-	update()
-	GPIO.add_event_detect(up, GPIO.RISING, callback=update)
-	GPIO.add_event_detect(down, GPIO.RISING, callback=update)
-	GPIO.add_event_detect(right, GPIO.RISING, callback=update)
-	GPIO.add_event_detect(left, GPIO.RISING, callback=update)
-	GPIO.add_event_detect(reset, GPIO.RISING, callback=update)
-
-
+while True:
+	time.sleep(100)
